8af8db488436bcfad1ce0c7bf3dbc282c345bd32
sergey <besaevso@novotelecom.ru>
2021-12-18 15:26:23 +0300

    commit

---
+++ src/main/java/com/example/polyglot/entity/Word.java
@@ -0,0 +1,20 @@
+package com.example.polyglot.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@SuperBuilder
+public class Word {
+
+    private String eng;
+    private String transcription;
+    private String rus;
+
+}
